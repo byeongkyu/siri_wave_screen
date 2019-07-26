aebfddd578673198021dbe502ea55367fdb62d3b
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2019-07-26 15:29:38 +0900

    add bringup launch & run with fullscreen default

--- nodejs/main.js
+++ nodejs/main.js
@@ -11,7 +11,7 @@ function createWindow () {
     })
 
     win.loadFile('index.html')
-    win.setFullScreen(false)
+    win.setFullScreen(true)
     // win.webContents.openDevTools()
 
     win.on('closed', () => {
